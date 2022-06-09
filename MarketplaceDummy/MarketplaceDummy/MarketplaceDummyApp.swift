e03e189ea8e606289bf1ba0eda04894318397d02
fgsoruco-redbee <fernando.soruco@externos-cl.cencosud.com>
2022-06-09 12:56:02 -0300

    Initial commit

---
+++ MarketplaceDummy/MarketplaceDummy/MarketplaceDummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketplaceDummyApp.swift
+//  MarketplaceDummy
+//
+//  Created by fgsoruco on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MarketplaceDummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
